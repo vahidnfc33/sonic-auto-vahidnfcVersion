dd6f8bfa8eda78ff4e35c0adad5441c8621f98e1
Shaho <shahooak@gmail.com>
2024-08-08 22:47:54 +0330

    Update displayUtils.js

--- src/displayUtils.js
+++ src/displayUtils.js
@@ -4,8 +4,8 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
   console.log('=           Sonic Odyssey BOT          ='.cyan);
-  console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-  console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
+  console.log('=     Edited by UbuntuForNodes         ='.cyan);
+  console.log('=    https://t.me/UbuntuForNodes       ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }
